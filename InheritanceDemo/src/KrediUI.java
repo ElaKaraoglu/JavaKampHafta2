a7eb256c2c11f2318622f3c423a602626c056793
ELA <ELA@LAPTOP-BK1LEHQM>
2022-09-29 15:23:47 +0300

    ödev2

---
+++ InheritanceDemo/src/KrediUI.java
@@ -0,0 +1,10 @@
+
+public class KrediUI {
+	 public void krediHesapla(BaseKrediManager kredimanager) 
+	 {
+	 kredimanager.hesapla();
+	 }
+		 
+}
+	 
+
---
+++ InheritanceDemo/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+      KrediUI krediui=new KrediUI();
+      krediui.krediHesapla(new TarımKrediManager());
+
+      
+	}
+
+}
